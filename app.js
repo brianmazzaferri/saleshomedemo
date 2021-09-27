aee85779e1e9b308b74aae9fec8fbe03b030e6ea
Brian Mazzaferri <brianmazzaferri@gmail.com>
2021-09-27 11:31:26 -0500

    add home listener

--- app.js
+++ app.js
@@ -58,7 +58,37 @@ const app = new App({
 
 //LISTENERS GO HERE
 
-
+app.event('app_home_opened', async ({ event, client }) => {
+  try {
+    const result = await client.views.publish({
+      // Use the user ID associated with the event
+      user_id: event.user,
+      view: {
+        // Home tabs must be enabled in your app configuration page under "App Home"
+        "type": "home",
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome home, <@" + event.user + "> :house:*"
+            }
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>."
+            }
+          }
+        ]
+      }
+    });
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
 
 //BOILERPLATE BELOW HERE
 
